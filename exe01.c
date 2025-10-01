33175bd177000ba287bd6ca9f9d0a5a27c959a97
Gabriel <gabriel.martins.gomes.11@gmail.com>
2025-10-01 12:40:37 -0300

    Delete exe01.c

--- exe01.c
+++
@@ -1,6 +0,0 @@
-/*O que são estruturas de dados e qual sua importância no desenvolvimento de software? 
-
-Estruturas de dados são formas específicas de organizar, gerenciar
-e armazenar dados em um computador para que eles possam ser usados de maneira eficiente. 
-Elas definem a forma como os dados são organizados, como podem ser acessados,
-modificados e manipulados.*/
